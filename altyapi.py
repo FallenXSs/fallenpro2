e3c963009f42bd8833eedd17a530a1ae5e08313d
FallenXSs <131010263+FallenXSs@users.noreply.github.com>
2023-09-27 20:43:59 +0400

    Update altyapi.py

--- altyapi.py
+++ altyapi.py
@@ -6,7 +6,7 @@ from telebot import types
 
 
 TOKEN = input("6086089724:AAELu6YRS_U0JuJMmWPyhtnBMWJ18iOnPRY")
-bot = telebot.TeleBot(TOKEN)
+bot = telebot.TeleBot("6086089724:AAELu6YRS_U0JuJMmWPyhtnBMWJ18iOnPRY")
 
 print(*"BOT AKTİF 🎉")
 
