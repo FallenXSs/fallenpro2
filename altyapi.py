d26ef24c64d4880f498cacae8672e6bd2a930cbf
FallenXSs <131010263+FallenXSs@users.noreply.github.com>
2023-09-28 17:59:59 +0400

    Update altyapi.py

--- altyapi.py
+++ altyapi.py
@@ -160,7 +160,7 @@ def tc(message):
 @bot.message_handler(commands=['admin'])
 def admin_command(message):
     user_id = message.from_user.id
-    if user_id in sudo_users OWNER_ID:
+    if user_id in sudo_users in OWNER_ID:
         bot.reply_to(message, 'Merhaba Yöneticim!💫 İşte komutlarınız:\n\n/wban - Kullanıcıyı Bottan Yasaklarım /adminekle - kullanici id /adminsil - kullanici id\n/unwban - Yasağı Kaldırırım\n/gen - Yeni Key Oluştururum')
     else:
         
